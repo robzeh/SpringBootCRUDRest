769532fd7e89f8498ee5d264c14115bea858ff9c
robzeh <jn912820@dal.ca>
2019-12-30 14:12:32 -0400

    Initial commit

---
+++ src/test/java/com/robie/springbootcrudrest/SpringbootcrudrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robie.springbootcrudrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcrudrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
